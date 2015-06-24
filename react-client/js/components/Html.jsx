d2eea546520920ae81834944d85027bbfe7c108b
Nick Massey <nickmass@nickmass.com>
2015-06-24 20:15:15 +0000

    Move cdn

--- react-client/js/components/Html.jsx
+++ react-client/js/components/Html.jsx
@@ -8,14 +8,14 @@ var Html = React.createClass({
 		<meta charSet="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<title>NickMass.com</title>
-		<link href="//static.nickmass.com/css/bundle.css" rel="stylesheet" type="text/css"/>
+		<link href="//cdn.nickmass.com/css/bundle.css" rel="stylesheet" type="text/css"/>
 		<link href="//fonts.googleapis.com/css?family=Josefin+Sans:300,400,700,400italic|Source+Code+Pro" rel="stylesheet" type="text/css"/>
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body dangerouslySetInnerHTML={{__html: this.props.markup}}>
 	</body>
 	<script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
-	<script src="//static.nickmass.com/js/bundle.js" defer></script>
+	<script src="//cdn.nickmass.com/js/bundle.js" defer></script>
 </html>
 			   );
 	}
